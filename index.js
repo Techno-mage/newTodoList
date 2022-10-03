384b4aecfd43fd031edcff1d3127a419b1da2f9b
Erik Schaal <schaa077@umn.edu>
2022-10-03 16:44:32 -0500

    started working on some JS

---
+++ index.js
@@ -0,0 +1,24 @@
+class notes{
+    constructor(title, message){
+        this.date = new Date();
+        this.title = title;
+        this.message = message
+    }
+}
+
+const ToDoHandler = {
+    toDoList: [],
+
+    
+};
+//Handle reads and writes to Local storage
+
+
+
+
+//display items on the page
+
+
+
+
+//TODO: handle the rendering of notes. 
