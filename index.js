c9434b43a7e804bf9740fb57cd8a39d4ad429b59
Erik Schaal <schaa077@umn.edu>
2022-10-18 11:38:01 -0500

    able to render content to the page

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ class notes{
 }
 
 const ToDoHandler = {
-    toDoList: [],
+    toDoList: [
+        "Review HTML, make this look good.",
+        "Review CSS, make this Responsive.",
+        "Review JS, give this some functionality."
+                ],
+
+    renderTodos: function(){
+        const list = document.getElementById('list');
+        let entry = "";
+        if (list){
+            for (item of this.toDoList){
+                entry = entry.concat(`<div><li>${item}</li></div>`)
+            }
+            list.innerHTML = entry;
+        }
+        
+        
+    },
+
+    
 
     
 };
+
+ToDoHandler.renderTodos();
 //Handle reads and writes to Local storage
 
 
